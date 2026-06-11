043c720a0d52578941dba8287e5bce542f04fe52
Ethan Winters <ethan.winters@gmail.com>
2026-06-11 11:40:50 -0400

    fix: processing failed with no loop value set
    Also, quickLoad wasn't actually implemented

--- packages/ai-chat-components/src/components/processing/__stories__/processing-react.stories.jsx
+++ packages/ai-chat-components/src/components/processing/__stories__/processing-react.stories.jsx
@@ -1,3 +1,12 @@
+/*
+ *  Copyright IBM Corp. 2025, 2026
+ *
+ *  This source code is licensed under the Apache-2.0 license found in the
+ *  LICENSE file in the root directory of this source tree.
+ *
+ *  @license
+ */
+
 /* eslint-disable */
 import React from "react";
 
@@ -19,6 +28,7 @@ const renderProcessing = (args) => (
 export const QuickLoad = {
   args: {
     quickLoad: true,
+    loop: true,
   },
   argTypes,
   render: renderProcessing,
--- packages/ai-chat/src/chat/components-legacy/MessagesTypingIndicator.tsx
+++ packages/ai-chat/src/chat/components-legacy/MessagesTypingIndicator.tsx
@@ -1,5 +1,5 @@
 /*
- *  Copyright IBM Corp. 2025
+ *  Copyright IBM Corp. 2025, 2026
  *
  *  This source code is licensed under the Apache-2.0 license found in the
  *  LICENSE file in the root directory of this source tree.
@@ -48,6 +48,7 @@ function MessagesTypingIndicator({
                 <Processing
                   className="cds-aichat--processing-component"
                   loop
+                  quickLoad
                   carbonTheme={carbonTheme}
                   aria-label={processingLabel}
                 />{" "}
